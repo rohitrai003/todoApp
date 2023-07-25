4835c7ccf7059bed9787560e61e5965f8b9e07fb
rohitrai003 <rbahingrai143@gmail.com>
2023-07-25 17:16:23 +0530

    changes in dialog box for adding task

---
+++ lib/Constant/Color.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color kPrimaryColor = const Color(0xFFC84646);
+Color kSecondaryColor = const Color(0xFFF6F3D8);
---
+++ lib/Constant/value.dart
@@ -0,0 +1,14 @@
+DateTime date = DateTime.now();
+List week = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+
+List<String> task = [];
+List<bool> taskCompleted = List.generate(20, (index) => false);
+List<String> mood = ['😀', '😒', '😍', '😴', '😡', '😱'];
